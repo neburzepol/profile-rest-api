e064f4c8ac048db43500f4fa2d081ca7060bc05e
Ali Lopez <lopez.galaviz@gmail.com>
2019-10-28 19:05:46 -0600

    Create profile item serializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -28,3 +28,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+class ProfileFeedItemSerializer(serialisers:ModelSerializer):
+    """ serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id','user_profile','status_text','created_on')
+        extra_kwargs = {
+            'user_profile': {'read_only': True}
+        }
